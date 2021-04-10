c34db0a2228eb37bf2067b1aab5680fcc52b5e57
Sean <jb@JBs-MacBook-Pro.local>
2021-04-10 13:17:52 -0500

    added color

--- src/javascripts/components/loginButton.js
+++ src/javascripts/components/loginButton.js
@@ -1,6 +1,5 @@
 import signIn from '../helpers/signIn';
 
-// GOOGLE LOGIN BUTTON
 const loginButton = () => {
   const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
   document.querySelector('#login-form-container').innerHTML = domString;
